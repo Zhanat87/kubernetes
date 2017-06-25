bf707c932f8c8a959d6f79c0b870a955ae3d4353
zhanat <iskakov_zhanat@mail.ru>
2017-06-25 10:20:46 +0600

    https://github.com/buaazp/fasthttprouter - too not active support best way for use fasthttp - it's use iris framework: https://github.com/kataras/iris

--- fasthttp/server.go
+++ fasthttp/server.go
@@ -9,12 +9,11 @@ import (
 	"os"
 	stdlog "log"
 	kitlog "github.com/go-kit/kit/log"
-	"github.com/Zhanat87/go/util"
 )
 
 var logger = logrus.New()
 
-func logrusHandler(c *routing.Context) error {
+func logrusMiddleware(c *routing.Context) error {
 	logger.Infof("logrusHandler path: %s. uri: %s", string(c.Path()), string(c.Request.RequestURI()))
 	//if len(c.Param("test")) > 0 {
 	//	logger.Infof("test param: %s", c.Param("test"))
@@ -22,33 +21,40 @@ func logrusHandler(c *routing.Context) error {
 	return nil
 }
 
-func goKitLogHandler(c *routing.Context) error {
+func goKitLogMiddleware(c *routing.Context) error {
 	logger := kitlog.NewJSONLogger(kitlog.NewSyncWriter(os.Stdout))
 	stdlog.SetOutput(kitlog.NewStdlibAdapter(logger))
 	stdlog.Printf("goKitLogHandler path: %s. uri: %s", string(c.Path()), string(c.Request.RequestURI()))
 	return nil
 }
 
+func helloWorldHandler(c *routing.Context) error {
+	fmt.Fprintf(c, "Hello, world!")
+	return nil
+}
+
+func testHandler(c *routing.Context) error {
+	fmt.Fprintf(c, "test action. test param value: %s", c.Param("test"))
+	return nil
+}
+
+func pingHandler(c *routing.Context) error {
+	fmt.Fprint(c, "pong")
+	return nil
+}
+
 func main() {
 	router := routing.New()
 
-	router.Use(logrusHandler)
+	router.Use(logrusMiddleware)
 
-	router.Use(goKitLogHandler)
+	router.Use(goKitLogMiddleware)
 
-	router.Get("/", func(c *routing.Context) error {
-		fmt.Fprintf(c, "Hello, world!")
-		return nil
-	})
+	router.Get("/", helloWorldHandler)
 
-	router.Get("/test/<test>", func(c *routing.Context) error {
-		fmt.Fprintf(c, "test action. test param value: %s", c.Param("test"))
-		return nil
-	})
+	router.Get("/test/<test>", testHandler)
 
-	router.Get("/json", func(c *routing.Context) error {
-		return c.Write(util.H{"a": "b", "c": 1})
-	})
+	router.Get("/ping", pingHandler)
 
 	panic(fasthttp.ListenAndServe(":8080", router.HandleRequest))
 }
